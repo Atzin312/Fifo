659dd6370af4d58cbd73fbb2ff66f91e56bd532d
Atzin312 <amoreno9@ucol.mx>
2017-06-13 00:18:43 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace FIFO
+{
+	partial class frmPrincipal
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.btnSimular = new System.Windows.Forms.Button();
+            this.txtConsola = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSimular
+            // 
+            this.btnSimular.Location = new System.Drawing.Point(13, 12);
+            this.btnSimular.Name = "btnSimular";
+            this.btnSimular.Size = new System.Drawing.Size(457, 35);
+            this.btnSimular.TabIndex = 0;
+            this.btnSimular.Text = "Simular Fifo";
+            this.btnSimular.UseVisualStyleBackColor = true;
+            this.btnSimular.Click += new System.EventHandler(this.btnSimular_Click);
+            // 
+            // txtConsola
+            // 
+            this.txtConsola.Location = new System.Drawing.Point(13, 54);
+            this.txtConsola.Multiline = true;
+            this.txtConsola.Name = "txtConsola";
+            this.txtConsola.Size = new System.Drawing.Size(471, 503);
+            this.txtConsola.TabIndex = 1;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 570);
+            this.Controls.Add(this.txtConsola);
+            this.Controls.Add(this.btnSimular);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "frmPrincipal";
+            this.Text = "Procesos FIFO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnSimular;
+		private System.Windows.Forms.TextBox txtConsola;
+	}
+}
+
---
+++ Simulador.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIFO
+{
+	class Simulador
+	{
+		private static Random rand;
+		Queue<Proceso> filaSimulacion;
+		private int procesosAten, ciclosVa, restantes, sumaRestantes;
+
+		public Simulador()
+		{
+			rand = new Random();
+			filaSimulacion = new Queue<Proceso>();
+		}
+
+		public String simular()
+		{
+			procesosAten = 0;
+			ciclosVa = 0;
+			sumaRestantes = 0;
+			for (int i = 0; i < 200; i++)
+			{
+				if (rand.Next(1,5)==3)
+				{
+					Proceso p = new Proceso(rand.Next(4,15));
+					filaSimulacion.Enqueue(p);
+				}
+				if (filaSimulacion.Count!=0)
+				{
+					filaSimulacion.Peek().ciclos--;
+					if (filaSimulacion.Peek().ciclos==0)
+					{
+						filaSimulacion.Dequeue();
+						procesosAten++;
+					}
+				}
+				else
+				{
+					ciclosVa++;
+				}
+			}
+			restantes = filaSimulacion.Count;
+			while (filaSimulacion.Count!=0)
+			{
+				sumaRestantes+= filaSimulacion.Dequeue().ciclos;
+			}
+			return "Procesos atendidos: " + procesosAten + Environment.NewLine + "Ciclos vacios: " + ciclosVa + Environment.NewLine + "Procesos faltantes: " + restantes + Environment.NewLine + "Suma restantes: " + sumaRestantes;
+		}
+	}
+}
